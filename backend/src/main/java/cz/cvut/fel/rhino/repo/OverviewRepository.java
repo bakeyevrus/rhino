dfb4d1921243c4eee5f4bdec3d040d36282212e9
Ruslan Bakeyev <bakeyevrus@gmail.com>
2019-03-02 15:46:19 +0100

    Remove old backend version

--- backend/src/main/java/cz/cvut/fel/rhino/repo/OverviewRepository.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fel.rhino.repo;
-
-import cz.cvut.fel.rhino.model.Overview;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OverviewRepository extends MongoRepository<Overview, String> {
-
-    Overview getFirstBy();
-}
