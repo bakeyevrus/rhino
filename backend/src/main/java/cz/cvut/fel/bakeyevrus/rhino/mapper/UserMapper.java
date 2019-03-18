de03d3b71aae9ad58b513c04282f175db3f1a07a
Ruslan Bakeyev <bakeyevrus@gmail.com>
2019-03-18 20:17:51 +0100

    Fix /login endpoints issues
    - Decode user password from base64 format
    - Wrap String message with ErrorResponseDto.java

--- backend/src/main/java/cz/cvut/fel/bakeyevrus/rhino/mapper/UserMapper.java
+++ backend/src/main/java/cz/cvut/fel/bakeyevrus/rhino/mapper/UserMapper.java
@@ -2,22 +2,18 @@ package cz.cvut.fel.bakeyevrus.rhino.mapper;
 
 import cz.cvut.fel.bakeyevrus.rhino.dto.UserRegisterRequestDto;
 import cz.cvut.fel.bakeyevrus.rhino.model.User;
+import cz.cvut.fel.bakeyevrus.rhino.util.PasswordDecoder;
 import org.springframework.stereotype.Component;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 
 @Component
 public class UserMapper {
 
     public User toUser(UserRegisterRequestDto newUser) {
-        var decodedPass = Base64.getDecoder().decode(newUser.getPassword());
         return new User(
                 newUser.getFirstName(),
                 newUser.getLastName(),
                 newUser.getEmail(),
-                // UTF-8 is default JSON encoding
-                new String(decodedPass, StandardCharsets.UTF_8)
+                PasswordDecoder.base64Decode(newUser.getPassword())
         );
     }
 }
