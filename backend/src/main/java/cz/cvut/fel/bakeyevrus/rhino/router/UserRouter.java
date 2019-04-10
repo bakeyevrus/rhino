a851639b7ab2045a08d6cfd66468dacc79504ef6
Ruslan Bakeyev <bakeyevrus@gmail.com>
2019-04-10 17:32:38 +0200

    Implement get project list endpoint (GET /project), refactor handler and service layers

--- backend/src/main/java/cz/cvut/fel/bakeyevrus/rhino/router/UserRouter.java
+++ backend/src/main/java/cz/cvut/fel/bakeyevrus/rhino/router/UserRouter.java
@@ -16,12 +16,14 @@ public class UserRouter {
 
     @Bean
     public RouterFunction<ServerResponse> route(UserHandler userHandler, ProjectHandler projectHandler) {
+        // TODO: extract routes to constants
         return RouterFunctions.route()
                 .POST("/register", accept(APPLICATION_JSON), userHandler::register)
                 .POST("/login", accept(APPLICATION_JSON), userHandler::login)
-                .POST("/api/v1/project", projectHandler::createProject)
+                .GET("/api/v1/project", projectHandler::getListOfProjects)
+                .POST("/api/v1/project", accept(APPLICATION_JSON), projectHandler::createProject)
                 .GET("/api/v1/project/{id}", projectHandler::getProjectById)
-                .PUT("/api/v1/project/{id}", projectHandler::updateProject)
+                .PUT("/api/v1/project/{id}", accept(APPLICATION_JSON), projectHandler::updateProject)
                 .DELETE("/api/v1/project/{id}", projectHandler::deleteProject)
                 .build();
     }
